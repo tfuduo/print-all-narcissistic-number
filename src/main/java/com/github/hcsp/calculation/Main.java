9e5ed0e3f01a2abb266ea3610ee9981069991b57
Bo Zhang <bo@gradle.com>
2019-06-08 17:53:45 +0800

    Init repo

---
+++ src/main/java/com/github/hcsp/calculation/Main.java
@@ -0,0 +1,16 @@
+package com.github.hcsp.calculation;
+
+public class Main {
+    public static void main(String[] args) {
+        printNarcissisticNumber();
+    }
+
+    /**
+     * 打印所有的水仙花数。水仙花数是指一个三位数，其各位数字立方和等于该数本身。
+     *
+     * <p>例如，153是一个水仙花数，因为1的立方方+5的立方+3的立方 = 153
+     *
+     * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
+     */
+    public static void printNarcissisticNumber() {}
+}
