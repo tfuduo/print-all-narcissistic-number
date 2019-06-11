c27cb92a2a5e3aef8d09286ecc8d57d564f6e932
吴奇 <908380434@qq.com>
2019-06-11 20:26:49 +0800

    Finish narcissus number generator (#2)
    Finish narcissus number generator (#2)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -6,11 +6,21 @@ public class Main {
     }
 
     /**
-     * 打印所有的水仙花数。水仙花数是指一个三位数，其各位数字立方和等于该数本身。
+     *  打印所有的水仙花数。水仙花数是指一个三位数，其各位数字立方和等于该数本身。
      *
-     * <p>例如，153是一个水仙花数，因为1的立方方+5的立方+3的立方 = 153
+     *  <p>例如，153是一个水仙花数，因为1的立方方+5的立方+3的立方 = 153
      *
-     * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
+     *  <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        int MAX = 999;
+        int MIN = 100;
+
+        for (int i = MIN; i <= MAX; i++) {
+            int num = (int)(Math.pow(i / 100 % 10, 3) + Math.pow(i / 10 % 10, 3) + Math.pow(i / 1 % 10, 3));
+            if (num == i) {
+                System.out.println(num);
+            }
+        }
+    }
 }
