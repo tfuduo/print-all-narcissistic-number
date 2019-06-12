c291d532069e0bbe03d03cae027fac9cb85adc3e
JayChenFE <chenjiehwy@163.com>
2019-06-12 13:55:48 +0800

    水仙花数 (#3)
    水仙花数 (#3)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.controlflow;
 
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         printNarcissisticNumber();
@@ -12,5 +14,28 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+
+        IntStream.rangeClosed(100, 999)
+                .filter(x -> isNarcissisticNumber(x))
+                .forEach(x -> System.out.println(x));
+    }
+
+    /* 判断value是否为水仙花数 */
+    public static boolean isNarcissisticNumber(int value) {
+        int temp = value;
+        int digits = 0;
+        // 判断value有几位数，保存在digits
+        while (temp > 0) {
+            digits++;
+            temp /= 10;
+        }
+        temp = value;
+        int sum = 0;
+        while (temp > 0) {
+            sum += Math.pow(temp % 10, digits);
+            temp /= 10;
+        }
+        return sum == value;
+    }
 }
