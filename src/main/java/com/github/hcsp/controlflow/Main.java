cf96be50c07e60bdb75a1388b3f98fdde66da5ea
黄楚才 <425205839@qq.com>
2019-06-12 15:42:14 +0800

    水仙数 (#4)
    水仙数 (#4)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,13 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        final int MaxNum = 999;
+        final int MinNum = 100;
+        for (int i = MinNum; i <= MaxNum; i++) {
+            if (i == Math.pow(i / 100 % 10, 3) + Math.pow(i / 10 % 10, 3) + Math.pow(i / 1 % 10, 3)) {
+                System.out.println(i);
+            }
+        }
+    }
 }
