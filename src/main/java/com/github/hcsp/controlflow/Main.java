fe08b801f3d222cc1a789e939a50bd9cb4478fa6
Li-saltair <huagongyilby@126.com>
2019-06-14 13:32:41 +0800

    水仙花数 (#6)
    水仙花数 (#6)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -8,9 +8,19 @@ public class Main {
     /**
      * 打印所有的水仙花数。水仙花数是指一个三位数，其各位数字立方和等于该数本身。
      *
-     * <p>例如，153是一个水仙花数，因为1的立方方+5的立方+3的立方 = 153
+     * <p>例如，153是一个水仙花数，因为1的立方+5的立方+3的立方 = 153
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        int hun,ten,unit;
+        for (int i = 100; i<1000; i++) {
+            hun = i/100;
+            ten = (i/10)%10;
+            unit = i%10;
+            if(Math.pow(hun,3) + Math.pow(ten,3) + Math.pow(unit,3) == i){
+                System.out.println(i);
+            }
+        }
+    }
 }
