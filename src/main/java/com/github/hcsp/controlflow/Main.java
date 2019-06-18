4bb545a268c81fa1ac0f92d3821aaa482bfa6848
余韵之 <36914785+Lambert1129@users.noreply.github.com>
2019-06-18 13:20:11 +0800

    x (#9)
    x (#9)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,18 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        int start = 100;
+        int end = 999;
+
+        for (int i=start;i<=end;i++){
+            int g=i%10;  // 个
+            int s = (int) Math.floor(i/10) % 10; // 十
+            int b = (int) Math.floor(i/100);  // 百
+            if(i ==(Math.pow(g,3)+Math.pow(s,3)+Math.pow(b,3))){
+                System.out.println(i);
+            }
+
+        }
+    }
 }
