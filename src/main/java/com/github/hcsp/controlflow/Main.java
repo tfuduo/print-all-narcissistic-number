c3c709a9b1ca8bbf867eec4d616416fb9623bd15
Tu Wenbin <949193531@qq.com>
2019-06-18 16:38:37 +0800

    [A]水仙花数 (#11)
    [A]水仙花数 (#11)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,14 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 999; i++) {
+            int a = i /100;
+            int b = (i - a*100)/10;
+            int c = (i - a*100 - b*10);
+            if(i == a*a*a + b*b*b + c*c*c) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 }
