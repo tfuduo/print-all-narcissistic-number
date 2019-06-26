a1de65d1219c87b33b97f5bfe320acb4eaabf6a8
XiongAmao <onycat@live.cn>
2019-06-26 22:51:53 +0800

    Print all narcissistic number.

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,19 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i <= 999; i++) {
+            int s = pow3(i % 10);
+            int t = pow3(i / 10 % 10);
+            int h = pow3(i / 100 % 10);
+
+            if ((s + t + h) == i) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static int pow3(int base) {
+        return (int) Math.pow(base, 3);
+    }
 }
