49c92d9b205f1fff8ef3235d57d295ef16420c37
fei tang <497045233@qq.com>
2019-08-01 15:12:04 +0800

    打印所有的水仙花数 (#35)
    打印所有的水仙花数 (#35)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,15 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i =100; i < 1000; i++) {
+            String str = String.valueOf(i);
+            int result=0;
+            for (int l = 0; l < str.length(); l++) {
+                String c = String.valueOf(str.charAt(l));
+                result+=Math.pow(Integer.parseInt(c), 3);
+            }
+            if(result==i) System.out.println(i);
+        }
+    }
 }
