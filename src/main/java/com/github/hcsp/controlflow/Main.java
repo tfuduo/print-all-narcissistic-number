18aa068b87679b8463b41c78187fe053a5dc85d9
taoxm310 <taoxiumin2016@gmail.com>
2019-08-01 22:29:17 +0800

    print-all-narcissistic-number (#36)
    print-all-narcissistic-number (#36)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,21 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+           int number = getCub(i);
+           if (number == i){
+               System.out.println(i);
+           }
+        }
+    }
+    public static int getCub(int digital) {
+        int result = 0;
+        for (int i = 1; i < 4; i ++) {
+            int a = digital % 10;
+            digital = (digital - a) / 10;
+            result += Math.pow(a, 3);
+        }
+        return result;
+    }
 }
