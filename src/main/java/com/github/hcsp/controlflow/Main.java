8511047843bb6e01e68b99f02167d4fda15b7800
Mingru Li <limingru1991@gmail.com>
2019-08-08 17:08:26 +0800

    查到所有三位的水仙花数字 (#40)
    查到所有三位的水仙花数字 (#40)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,16 +1,29 @@
 package com.github.hcsp.controlflow;
 
 public class Main {
-    public static void main(String[] args) {
-        printNarcissisticNumber();
-    }
+	public static void main(String[] args) {
+		printNarcissisticNumber();
+	}
 
-    /**
-     * 打印所有的水仙花数。水仙花数是指一个三位数，其各位数字立方和等于该数本身。
-     *
-     * <p>例如，153是一个水仙花数，因为1的立方+5的立方+3的立方 = 153
-     *
-     * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
-     */
-    public static void printNarcissisticNumber() {}
+	/**
+	 * 打印所有的水仙花数。水仙花数是指一个三位数，其各位数字立方和等于该数本身。
+	 *
+	 * <p>例如，153是一个水仙花数，因为1的立方+5的立方+3的立方 = 153
+	 *
+	 * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
+	 */
+	public static void printNarcissisticNumber() {
+		for (int i = 100; i <= 999; ++i) {
+            int units = i % 10;
+            int tens = (i % 100 - units) / 10;
+            int hundreds = i / 100;
+            if (cube(units) + cube(tens) + cube(hundreds) == i) {
+	            System.out.println(i);
+            }
+		}
+	}
+
+	public static int cube(int number) {
+		return (int)Math.pow(number, 3);
+	}
 }
