ba19f7bf293237b5cd993e3f8b6e7e12bac8ea53
shenwl <shenweilun96@gmail.com>
2019-08-11 21:17:58 +0800

    打印所有的水仙花数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,15 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for(int i = 100; i <= 999; i++) {
+            int unit = i % 100 % 10;
+            int decade = i / 10 % 10;
+            int hundred = i / 100;
+
+            if(Math.pow(unit, 3) + Math.pow(decade, 3) + Math.pow(hundred, 3) == i) {
+                System.out.println(i);
+            }
+        }
+    }
 }
