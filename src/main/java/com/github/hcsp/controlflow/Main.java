109b8281739be6db4e1b475e7285f990e050a4e6
PingAn_NetBar <840495823@qq.com>
2019-08-14 00:07:53 +0800

    没分割，直接暴力循环hhh (#43)
    没分割，直接暴力循环hhh (#43)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,18 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+
+        for (int i = 1; i<= 9; i++){
+
+            for (int j = 0;j<=9;j++) {
+
+                    for (int k = 0;k<=9;k++){
+                        if(i*100+j*10+k == (i*i*i)+(j*j*j)+(k*k*k)){
+                            System.out.println(i*100+j*10+k);
+                        }
+                    }
+            }
+        }
+    }
 }
