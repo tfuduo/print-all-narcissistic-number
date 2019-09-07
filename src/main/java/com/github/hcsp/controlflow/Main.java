b330da817c0bc4c4cca42d03def342746950471d
42 <LiYingjie@protonmail.com>
2019-09-08 00:06:08 +0800

    feat: print narcissistic numbers (#55)
    feat: print narcissistic numbers (#55)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,14 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int num = 100; num < 1000; num++) {
+            int unit = num % 100 % 10, decade = num / 10 % 10, hundred = num / 100;
+
+            if (Math.pow(unit, 3) + Math.pow(decade, 3) + Math.pow(hundred, 3) == num) {
+                System.out.println(num);
+            }
+        }
+
+    }
 }
