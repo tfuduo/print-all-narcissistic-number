370404feac77277978182406a2fb47a9a0d415dd
wpsumsun <wpsumsun@126.com>
2019-09-19 19:33:53 +0800

    print all narcissistic number (#59)
    print all narcissistic number (#59)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,14 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int hundreds = i / 100;
+            int tens = (i - hundreds * 100) / 10;
+            int units = i % 10;
+            if (Math.pow(hundreds, 3) + Math.pow(tens, 3) + Math.pow(units, 3) == i) {
+                System.out.println(i);
+            }
+        }
+    }
 }
