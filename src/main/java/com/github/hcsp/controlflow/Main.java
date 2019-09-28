25febcbdded18ee9fb2e5130bb433ac5176b92d5
Sean654343159 <654343159@qq.com>
2019-09-28 22:35:27 +0800

    debug

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,14 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int bai = i / 100;
+            int shi = i % 100 / 10;
+            int ge = i % 10;
+            if (bai * bai * bai + shi * shi * shi + ge * ge * ge == bai * 100 + shi * 10 + ge) {
+                System.out.println(i);
+            }
+        }
+    }
 }
