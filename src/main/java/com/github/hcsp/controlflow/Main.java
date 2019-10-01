d967807b7413f513f5d42e8dc883bbca5b7b37f9
qiaomengnan16 <285921957@qq.com>
2019-10-02 07:56:52 +0800

    Number (#65)
    Number (#65)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,16 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for(int i = 100; i<= 999;i++) {
+            String [] string = (i + "").split("");
+            int count = 0;
+            for(String temp : string) {
+                int tempInt = Integer.parseInt(temp);
+                count += (tempInt * tempInt * tempInt);
+            }
+            if(count == i)
+                System.out.println(i);
+        }
+    }
 }
