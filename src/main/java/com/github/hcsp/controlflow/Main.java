b35bc2c4f9623ca0ba7d5ac04f0cd363c96c8a9c
Llane00 <bluetinestyle@126.com>
2019-10-22 11:29:09 +0800

    add: 完善printNarcissisticNumber函数 (#77)
    add: 完善printNarcissisticNumber函数 (#77)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,11 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i <= 999; i++) {
+            if (i == Math.pow(i / 100, 3) + Math.pow(i % 100 / 10, 3) + Math.pow(i % 100 % 10, 3)) {
+                System.out.println(i);
+            }
+        }
+    }
 }
