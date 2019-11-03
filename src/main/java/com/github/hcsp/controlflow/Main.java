cd8a3388990e2e4f4ab7eef766d6f9a2e48c5adb
Ditto0 <46198155+Ditto0@users.noreply.github.com>
2019-11-03 16:30:40 +0800

    1 (#82)
    1 (#82)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,16 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for(int i =100;i<=999; i++){
+            int temp=i;
+            int sum = 0;
+            for(int j=0; j<3; j++){
+                sum +=  (temp%10)*(temp%10)*(temp%10);
+                temp /=10;
+            }
+            if(sum==i)
+                System.out.println(i);
+        }
+    }
 }
