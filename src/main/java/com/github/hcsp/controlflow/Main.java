39022c9a6567d1cfc5d3d70b1ec07d58e820347b
tan661 <lucky.tanduzhi@gmail.com>
2019-11-28 23:29:19 +0800

    自恋数😂 (#87)
    自恋数😂 (#87)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,15 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int units = i %  10;
+            int tens = i / 10 % 10;
+            int hundreds = i / 100;
+            Double result = Math.pow(units, 3) + Math.pow(tens, 3) + Math.pow(hundreds, 3);
+            if(result.intValue() == i) {
+                System.out.println(i);
+            }
+        }
+    }
 }
