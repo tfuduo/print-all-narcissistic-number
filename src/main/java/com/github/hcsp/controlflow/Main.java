ab15feca44e1a2c3c115b24a629face0312b9b9a
Maricaya <43136257+Maricaya@users.noreply.github.com>
2019-12-02 14:44:45 +0800

    feat: 打印出所有水仙花 (#90)
    feat: 打印出所有水仙花 (#90)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,15 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        int i = 999;
+        while (i-->100) {
+            int hundreds = i / 100;
+            int tens = (i - hundreds * 100) / 10;
+            int single = (i - hundreds * 100) % 10;
+            if (hundreds*hundreds*hundreds + tens*tens*tens + single*single*single == i) {
+                System.out.println(i);
+            }
+        }
+    }
 }
