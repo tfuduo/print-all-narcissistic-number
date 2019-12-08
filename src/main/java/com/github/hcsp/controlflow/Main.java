d661668706c97a18ededb8223ce5e0b4f3ce1891
TangsongyuanmingQingHad <49867878+TangsongyuanmingQingHad@users.noreply.github.com>
2019-12-08 23:23:00 +0800

    shuixianhua (#95)
    shuixianhua (#95)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,18 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        int baiwei = 0;
+        int shiwei = 0;
+        int gewei = 0;
+        for (int i = 100; i < 1000 ; i++) {
+            gewei = i % 100 % 10 ;
+            shiwei = i/10%10;
+            baiwei = i/100;
+
+            if(i == Math.pow(gewei,3)+Math.pow(shiwei,3)+Math.pow(baiwei,3)) {
+                System.out.println(i);
+            }
+        }
+    }
 }
