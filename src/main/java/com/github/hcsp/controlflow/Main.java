64e2a36e941b503514be64a02fe3a803b424e65f
monalisali <34482115+monalisali@users.noreply.github.com>
2019-12-08 12:25:18 +0800

    print number (#94)
    print number (#94)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,19 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for(int i=1;i<=9;i++)
+        {
+            for(int j = 0; j<=9; j++)
+            {
+                for(int x = 0; x <=9; x++)
+                {
+                    double y = Integer.valueOf(String.valueOf(i) + String.valueOf(j) + String.valueOf(x));
+                    if(Math.pow(i,3) + Math.pow(j,3) + Math.pow(x,3) -  y < 1e-6){
+                        System.out.println(String.valueOf((int)y));
+                    }
+                }
+            }
+        }
+    }
 }
