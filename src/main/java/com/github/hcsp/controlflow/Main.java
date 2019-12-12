adb3d289f0d2d82506fa14fbf1d4eb261582cbad
yaohengfeng <1065435454@qq.com>
2019-12-12 16:00:46 +0800

    水仙花数 (#96)
    水仙花数 (#96)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,17 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int num = i;
+            int a = num / 100;
+            num = num % 100;
+            int b = num / 10;
+            num = num % 10;
+            int c = num;
+            if (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3) == i) {
+                System.out.println(i);
+            }
+        }
+    }
 }
