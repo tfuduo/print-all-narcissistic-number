e6fb3d33da58b0cb303f645ba211dea3d61b890a
xdcgh <44773625+xdcgh@users.noreply.github.com>
2019-12-30 14:59:48 +0800

    第二版，参照优秀答案修改。 (#109)
    第二版，参照优秀答案修改。 (#109)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.controlflow;
 
+import static java.lang.Math.*;
+import static java.lang.Math.pow;
+
 public class Main {
     public static void main(String[] args) {
         printNarcissisticNumber();
@@ -12,5 +15,25 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        /*
+          看了优秀答案后
+          再一次惊艳到我了，之前在学校里解答水仙花数时
+          我就是上一版的写法完成的，完成后就沾沾自喜，没想到还有更好的解法
+          而且我还没仔细读好题目，题目指明说了，水仙花数是指一个三位数
+          我还在，从i=1开始，真是羞耻万分
+          以下参考优秀答案修改
+         */
+
+        final int MAX = 999;
+        final int MIN = 100;
+
+        for (int i = MIN; i < MAX; i++) {
+            int num = (int) (pow(i / 100 % 10, 3) + pow(i / 10 % 10, 3) + pow(i / 1 % 10, 3));
+            if (num == i) {
+                System.out.println(num);
+            }
+        }
+
+    }
 }
