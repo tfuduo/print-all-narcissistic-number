6c9f130a11b925147232cb2d79a947db2fae4fed
zaker-666 <1075151259china@gmail.com>
2020-01-01 21:50:10 +0800

    水仙花数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,14 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
-}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int baiwei = i / 100;
+            int shiwei = i / 10 % 10;
+            int gewei = i % 10;
+            if (baiwei * baiwei * baiwei + shiwei * shiwei * shiwei + gewei * gewei * gewei == i) {
+                System.out.println(i + "是水仙花数");
+            }
+        }
+        }
+    }
