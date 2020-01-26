e24f19a0e40d9bcafa23ce3b76ace110039b2dcb
天天天天天 <1500846601@qq.com>
2020-01-26 15:10:13 +0800

    打印所有水仙花数 (#127)
    打印所有水仙花数 (#127)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,16 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        int i = 100;
+        for (int j = i; j <= 999; j++) {
+            int hundred = j / 100;
+            int ten = j % 100 / 10;
+            int digits = j % 10;
+            if (j == hundred * hundred * hundred + ten * ten * ten + digits * digits * digits) {
+                System.out.println(j);
+            }
+        }
+
+    }
 }
