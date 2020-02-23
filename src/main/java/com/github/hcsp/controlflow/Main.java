95a019e0b2b46656c601753485fcbae9dc37ac56
KisetsuLee <42855253+KisetsuLee@users.noreply.github.com>
2020-02-23 18:10:35 +0800

    打印水仙花数字 (#138)
    打印水仙花数字 (#138)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,18 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i <= 999; i++) {
+            int baiwei = i / 100;
+            int shiwei = i % 100 / 10;
+            int gewei = i % 10;
+            if (cube(baiwei) + cube(shiwei) + cube(gewei) == i) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static int cube(int num) {
+        return num * num * num;
+    }
 }
