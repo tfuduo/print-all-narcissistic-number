5b8ce61d87f56b288b7e1a6e1ab73ea6525b3c13
ARKbreeze <61083620+ARKbreeze@users.noreply.github.com>
2020-02-26 00:52:08 +0800

    完成水仙数的输出 (#140)
    完成水仙数的输出 (#140)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,15 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i <= 999; i++) {
+            //取几位 （先/几*10）%10  Math.pow
+            int one = i % 10;
+            int two = (i / 10) % 10;
+            int three = i / 100;
+            if ((Math.pow(one,3)+Math.pow(two,3)+Math.pow(three,3)) == i) {
+                System.out.println(i);
+            }
+        }
+    }
 }
