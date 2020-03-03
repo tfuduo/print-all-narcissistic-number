4d7b5b273e4ce37d73a8092a7b54fa6574501878
Truthcode <61576871+Truthcode@users.noreply.github.com>
2020-03-03 17:37:34 +0800

    aaa (#146)
    aaa (#146)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,14 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for(int i = 100;i <= 999;i++){
+            int bai = i / 100;
+            int shi = i / 10 % 10;
+            int ge = i % 10;
+            if (bai*bai*bai + shi*shi*shi + ge*ge*ge == i){
+                System.out.println(i);
+            }
+        }
+    }
 }
