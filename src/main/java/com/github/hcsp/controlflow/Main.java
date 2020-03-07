e6fa9028e637a8057c3ff370e08a526626127d2d
jslyq <61300836+jslyq@users.noreply.github.com>
2020-03-07 16:43:24 -0500

    水仙花数 (#148)
    水仙花数 (#148)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,15 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
-}
+    public static void printNarcissisticNumber() {
+        for (int units = 0; units <= 9; units++) {
+            for (int tens = 0; tens <= 9; tens++) {
+                for (int hundreds = 1; hundreds <= 9; hundreds++) {
+                    if(Math.pow(units,3)+Math.pow(tens,3)+Math.pow(hundreds,3)==units+tens*10+hundreds*100){
+                        System.out.println(units+tens*10+hundreds*100);
+                    }
+                }
+                }
+            }
+        }
+    }
