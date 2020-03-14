6a0f2899dce6cb06c68e233f913a4a9702b3ba74
liulian1999 <61763395+liulian1999@users.noreply.github.com>
2020-03-13 19:45:48 -0700

    number (#152)
    number (#152)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,15 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for(int i = 0; i <= 9; i++){
+            for(int j = 0; j <= 9; j++){
+                for (int k = 0; k <= 9; k++){
+                    if(Math.pow(i , 3) + Math.pow(j , 3) + Math.pow(k , 3) == i + (j * 10) + (k *100)){
+                        System.out.println(i+ (j * 10) + (k * 100));
+                    }
+                }
+            }
+        }
+    }
 }
