6669e6eb16132ad10dc6ab0a5c08dc395bb87650
gofocus <go_focus@live.cn>
2020-03-21 17:08:09 +0800

    打印所有的水仙花数 (#160)
    打印所有的水仙花数 (#160)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,19 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int a = i / 100;
+            int b = i % 100 / 10;
+            int c = i % 10;
+            if (lifang(a) + lifang(b) + lifang(c) == i) {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+    public static int lifang(int a) {
+        return a * a * a;
+    }
 }
