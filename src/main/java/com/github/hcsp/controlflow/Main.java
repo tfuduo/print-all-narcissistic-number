c0d091d67445b81cb3810cf2a6fdad7bef78a136
Huang xuewei <770342643@qq.com>
2020-03-26 00:33:09 +0800

    add print narcissistic number codes in printNarcissisticNumber method (#163)
    add print narcissistic number codes in printNarcissisticNumber method (#163)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,17 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        for (int i = 100; i < 1000; i++) {
+            int hundreds = i / 100;
+            int decade = (i / 10) % 10;
+            int singleDigit = i % 10;
+            int cubesSum = hundreds * hundreds * hundreds +
+                decade * decade * decade +
+                singleDigit * singleDigit * singleDigit;
+            if (i == cubesSum) {
+                System.out.println(i);
+            }
+        }
+    }
 }
