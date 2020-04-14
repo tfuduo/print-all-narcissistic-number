49a2d61ad5f0eaea252443d36812d03fae78e2cb
hue li <49891815+huesf@users.noreply.github.com>
2020-04-14 17:33:22 +0800

    3位数的水仙花数 (#168)
    3位数的水仙花数 (#168)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,5 +12,15 @@ public class Main {
      *
      * <p>提示：可用除法和求余运算得到一个数字的个、十、百位上的数字。
      */
-    public static void printNarcissisticNumber() {}
+    public static void printNarcissisticNumber() {
+        int n1,n2,n3;
+        for(int i=100;i<999;i++){
+            n1 = i/100;
+            n2= i%100/10;
+            n3=i %10;
+            if(n1*n1*n1 + n2*n2*n2 +n3*n3*n3 == i){
+                System.out.println(i);
+            }
+        }
+    }
 }
